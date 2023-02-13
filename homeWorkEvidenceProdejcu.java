03371fe531600679340f97f5fa6b61e133bf2998
xDavidxCz <zondra.david@hotmail.com>
2023-02-13 20:20:32 +0100

    Add files via upload

---
+++ homeWorkEvidenceProdejcu.java
@@ -0,0 +1,28 @@
+
+import java.time.LocalDate;import java.net.Authenticator;
+
+public class homeWorkEvidenceProdejcu {
+    public static void main(String[] args) {
+
+        String name;
+        LocalDate dateOfBirth;
+        int numberOfContracts;
+        double soldCarrotsInTone;
+        String place;
+        String registrationPlate;
+        double fuelUsagePer100km;
+        String Inet4Addres;
+
+        name = ("Honza Novák");
+        dateOfBirth = LocalDate.of(10,04,1980);
+        numberOfContracts = 5;
+        soldCarrotsInTone = 1.5;
+        place = ("Olomouc");
+        registrationPlate = ("5M4 7451");
+        fuelUsagePer100km = 6.5;
+        Inet4Addres = ("192.168.24.0");
+
+
+
+    }
+}
